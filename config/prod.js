0b60b289f57c6c1a7615ff669cff5e984f19a8df
Atar Mor <atarmor92@gmail.com>
2024-05-21 13:37:41 +0300

    Init-repo

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://atarmor92:FDby$wmDCTCu6wZ@cluster0.e3x2fqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbName : process.env.DB_NAME || 'expenseDB'
+}
