faf63a66508255c20c3b52c7db656974ff304342
Atar Mor <atarmor92@gmail.com>
2024-05-23 22:56:56 +0300

    Change config to use environment vars

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 export default  {
-  dbURL: 'mongodb+srv://atarmor92:FDby$wmDCTCu6wZ@cluster0.e3x2fqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: process.env.MONGO_CONNECTION_STR || 'mongodb://127.0.0.1:27017',
   dbName : 'codeblockDB'
 }
